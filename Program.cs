3320936d8f3d9b13ddf7886f16be19ca73d79b72
Odhran <mahonyodhran@gmail.com>
2022-06-27 13:23:23 +0100

    basic number guesser game - no validation

--- Program.cs
+++ Program.cs
@@ -5,7 +5,27 @@ namespace cSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random random = new Random();
+
+            int randomNumber = random.Next(0, 10);
+            bool isCorrect = false;
+
+            do
+            {
+                Console.Write("Guess a number between 0 - 10: ");
+                String guess = Console.ReadLine();
+                int i = int.Parse(guess);
+
+                if (i == randomNumber){
+                    Console.Write("Correct! Thanks for playing.");
+                    isCorrect = true;
+                }else if (i < randomNumber){
+                    Console.Write("Too low!\n");
+                }else{
+                    Console.Write("Too high!\n");
+                }
+                
+            } while (!isCorrect);
         }
     }
 }
